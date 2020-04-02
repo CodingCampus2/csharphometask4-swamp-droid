243ea538ff791148e353c5905b837d4540191351
Mykhailo Danilchenko <mykhailo.danilchenko@ubisoft.com>
2020-04-02 09:44:59 +0300

    add function

--- Program.cs
+++ Program.cs
@@ -5,6 +5,29 @@ namespace HomeworkTemplate
 {
     class Program
     {
+
+        static int GetAliveCellsCount(int x, int y, char[,] board)
+        {
+            int counter = 0;
+            for (int j = -1; j <= 1; j++)
+            {
+                for (int i = -1; i <= 1; i++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+
+                    if (board[x + i, y + j] == '1')
+                    {
+                        ++counter;
+                    }
+                }
+            }
+
+            return counter;
+        }
+
         static void Main(string[] args)
         {
             Func<Task4, char[,]>TaskSolver = task =>
@@ -13,9 +36,9 @@ namespace HomeworkTemplate
                 // You can get all needed inputs from task.[Property]
                 // Good luck!
                 char[,] board = task.Board;
+
                 int x = board.GetLength(0);
                 int y = board.GetLength(1);
-
                 char[,] virtualBoard = new char[x + 2, y + 2];
 
                 for(int i = 1; i <= y; i++)
@@ -30,33 +53,17 @@ namespace HomeworkTemplate
 
                 for(int i = 1; i <= y; i++)
                 {
-                    
                     for(int j = 1; j <= x; j++)
                     {
-                        int counter = 0;
-                        for(int a = -1; a <= 1; a++)
-                        {
-                            for(int b = -1; b <= 1; b++)
-                            {
-                                if(a == 0 && b == 0)
-                                {
-                                    continue;
-                                }
-
-                                if(virtualBoard[j+b,i+a] == '1')
-                                {
-                                    ++counter;
-                                }
-                            }
-                        }
-
+                        int aliveCells = GetAliveCellsCount(j, i, virtualBoard);
+                        
                         if(virtualBoard[j,i] == '1'
-                           && (counter == 2 || counter == 3))
+                           && (aliveCells == 2 || aliveCells == 3))
                         {
                             answerBoard[j - 1, i - 1] = '1';
                         }
                         else if (virtualBoard[j, i] == '0'
-                                && counter == 3)
+                                && aliveCells == 3)
                         {
                             answerBoard[j - 1, i - 1] = '1';
                         }
